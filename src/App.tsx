f8776d6d5105455943f267d11b442fe70e94ec7f
Evgeny Lahav <Evgeny_Lahav@amat.com>
2020-03-27 18:43:34 +0300

    added material UI support to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1)
+    }
+  }
+}));
 
 function App() {
-  const [currentTime, setCurrentTime ] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const classes = useStyles();
+
   useEffect(() => {
-    fetch('/time')
-    .then(res => res.json())
-    .then(data => {
-      setCurrentTime(data.time)
-    })
+    fetch("/time")
+      .then(res => res.json())
+      .then(data => {
+        setCurrentTime(data.time);
+      });
   }, []);
   return (
-    <div className="App">
+    <div className={classes.root}>
       <header className="App-header">
         <p>The current time is: {currentTime}</p>
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
       </header>
     </div>
   );
