c7c15026b8ad8597da8b73c639c78b135c3f4091
evgenylahav <evgeny.lahav@gmail.com>
2020-03-29 00:05:04 +0300

    small fix to enable google maps usage

--- src/components/pages/itinerary/itineraryPage.tsx
+++ src/components/pages/itinerary/itineraryPage.tsx
@@ -20,7 +20,7 @@ export class Itinerary extends React.Component<ItineraryProps, ItineraryState> {
         <WrappedMap
           googleMapURL={
             `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=
-            geometry,drawing,places&key=AIzaSyDCOzagzkQXlv7tDx4DTXFs2MdF6wBD1Sw`
+            geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`
           }
           loadingElement={<div style={{ height: "100%" }} />}
           containerElement={<div style={{ height: "100%" }} />}
