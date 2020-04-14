853d7e05496476b71c370206ed4073a6c4c47c0f
evgenylahav <evgeny.lahav@gmail.com>
2020-04-14 21:16:39 +0300

    fixed a small bug in pymongo - now load and save work properly

--- api/travelplanner/main.py
+++ api/travelplanner/main.py
@@ -20,9 +20,6 @@ def get_current_time():
 
 @main.route('/update_my_itinerary', methods=["POST"])
 def update_my_itinerary():
-    # get request
-    # update the database
-    # return status
     req_data = request.get_json()
 
     my_itinerary = mongo.db.my_itinerary
@@ -35,28 +32,6 @@ def update_my_itinerary():
 @main.route('/get_my_itinerary', methods=["GET"])
 def get_my_itinerary():
     my_itinerary = mongo.db.my_itinerary
-    l = my_itinerary.find()
+    l = my_itinerary.find({}, {'_id': False})
     response = dumps(l)
     return response
-
-    # return jsonify(response)
-
-    # result = [{"dayName": "Day 1",
-    #           "places": [{"id": 1,
-    #                       "name": "Amsterdam",
-    #                       "sleeping": False,
-    #                       "position": {"lat": 43.7102, "lng": 7.262},
-    #                       "day": "Day 1",
-    #                       "description": "football match",
-    #                       "media": "https://www.chamonix.com/img/sitra/fiche/151128-0-ot-vallee-de-chamonix-salome-abrial-0857.jpg",
-    #                       "web": "https://www.chamonix.com/chamonix-mont-blanc,0,en.html"}]},
-    #           {"dayName": "Day 2",
-    #            "places": [{"id": 2,
-    #                        "name": "Rotterdam",
-    #                        "sleeping": False,
-    #                        "position": {"lat": 53.7002, "lng": 5.362},
-    #                        "day": "Day 2",
-    #                        "description": "architecture",
-    #                        "media": "https://www.chamonix.com/img/sitra/fiche/151128-0-ot-vallee-de-chamonix-salome-abrial-0857.jpg",
-    #                        "web": "https://www.chamonix.com/chamonix-mont-blanc,0,en.html"}]}
-    #           ]
